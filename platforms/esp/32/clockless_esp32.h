e6da8a06ed8a2173addc0ab41792dcbdfa8a872e
Thomas Basler <thomas@familie-basler.net>
2017-10-07 11:41:15 +0200

    Applied changes made by @samguyer

--- platforms/esp/32/clockless_esp32.h
+++ platforms/esp/32/clockless_esp32.h
@@ -62,199 +62,191 @@ static uint8_t rmt_channels_used = 0;
 template <int DATA_PIN, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 5>
 class ClocklessController : public CPixelLEDController<RGB_ORDER>
 {
-	intr_handle_t rmt_intr_handle = NULL;
-	uint16_t T0H, T1H, T0L, T1L;
+    rmt_item32_t mZero;
+    rmt_item32_t mOne;
+
+    rmt_channel_t mRMT_channel;
+    xSemaphoreHandle mTX_sem = NULL;
+    intr_handle_t mRMT_intr_handle = NULL;
+
+    PixelController<RGB_ORDER> *local_pixels  = NULL;
+    uint8_t mRGB_channel;
+    uint16_t mCurPulse;
 
 public:
 
-	uint16_t buf_pos, buf_len, buf_half, buf_isDirty;
-	xSemaphoreHandle sem = NULL;
-	rmt_item32_t bitval_to_rmt_map[2];
-	uint16_t TRS;
-	uint8_t rmt_channel;
-	PixelController<RGB_ORDER> *local_pixels;
+    virtual void init()
+    {
+	// TRS = 50000;
 
-	virtual void init()
-	{
-		TRS = 50000;
-		T0H = CLKS_TO_NS(T1);
-		T1H = CLKS_TO_NS(T1 + T2);
+	// -- Precompute rmt items corresponding to a zero bit and a one bit
+	//    according to the timing values given in the template instantiation
+	mOne.level0 = 1;
+	mOne.duration0 = CLKS_TO_NS(T1 + T2) / (RMT_DURATION_NS * DIVIDER);
+	mOne.level1 = 0;
+	mOne.duration1 = CLKS_TO_NS(T3) / (RMT_DURATION_NS * DIVIDER);
 
-		T0L = CLKS_TO_NS(T2 + T3);
-		T1L = CLKS_TO_NS(T3);
+	mZero.level0 = 1;
+	mZero.duration0 = CLKS_TO_NS(T1) / (RMT_DURATION_NS * DIVIDER);
+	mZero.level1 = 0;
+	mZero.duration1 = CLKS_TO_NS(T2 + T3) / (RMT_DURATION_NS * DIVIDER);
 
-		DPORT_SET_PERI_REG_MASK(DPORT_PERIP_CLK_EN_REG, DPORT_RMT_CLK_EN);
-		DPORT_CLEAR_PERI_REG_MASK(DPORT_PERIP_RST_EN_REG, DPORT_RMT_RST);
+	// -- Sequentially assign RMT channels -- at most 8
+	mRMT_channel =  (rmt_channel_t) rmt_channels_used++;
+	if (mRMT_channel > 7) {
+	    assert("Only 8 RMT Channels are allowed");
+	}
 
-		rmt_channel = rmt_channels_used++;
-		if (rmt_channel > 7) {
-			assert("Only 8 RMT Channels are allowed");
-		}
+	ESP_LOGI("fastled", "RMT Channel Init: %d", mRMT_channel);
 
-		ESP_LOGI("fastled", "RMT Channel Init: %d", rmt_channel);
+	// -- RMT set up magic
+	DPORT_SET_PERI_REG_MASK(DPORT_PERIP_CLK_EN_REG, DPORT_RMT_CLK_EN);
+	DPORT_CLEAR_PERI_REG_MASK(DPORT_PERIP_RST_EN_REG, DPORT_RMT_RST);
 
-		rmt_set_pin(static_cast<rmt_channel_t>(rmt_channel),
-			RMT_MODE_TX,
-			static_cast<gpio_num_t>(DATA_PIN));
+	rmt_set_pin(static_cast<rmt_channel_t>(mRMT_channel),
+		    RMT_MODE_TX,
+		    static_cast<gpio_num_t>(DATA_PIN));
 
-		initRMTChannel(rmt_channel);
+	RMT.apb_conf.fifo_mask = 1;  //enable memory access, instead of FIFO mode.
+	RMT.apb_conf.mem_tx_wrap_en = 1; //wrap around when hitting end of buffer
 
-		RMT.tx_lim_ch[rmt_channel].limit = MAX_PULSES;
+	RMT.conf_ch[mRMT_channel].conf0.div_cnt = DIVIDER;
+	RMT.conf_ch[mRMT_channel].conf0.mem_size = 1;
+	RMT.conf_ch[mRMT_channel].conf0.carrier_en = 0;
+	RMT.conf_ch[mRMT_channel].conf0.carrier_out_lv = 1;
+	RMT.conf_ch[mRMT_channel].conf0.mem_pd = 0;
+	RMT.conf_ch[mRMT_channel].conf1.rx_en = 0;
+	RMT.conf_ch[mRMT_channel].conf1.mem_owner = 0;
+	RMT.conf_ch[mRMT_channel].conf1.tx_conti_mode = 0;    //loop back mode.
+	RMT.conf_ch[mRMT_channel].conf1.ref_always_on = 1;    // use apb clock: 80M
+	RMT.conf_ch[mRMT_channel].conf1.idle_out_en = 1;
+	RMT.conf_ch[mRMT_channel].conf1.idle_out_lv = 0;
 
-		RMT.int_ena.val |= BIT(24 + rmt_channel); // set ch*_tx_thr_event
-		RMT.int_ena.val |= BIT(rmt_channel * 3); // set ch*_tx_end
+	RMT.tx_lim_ch[mRMT_channel].limit = MAX_PULSES;
 
-		// RMT config for bit val 0
-		bitval_to_rmt_map[0].level0 = 1;
-		bitval_to_rmt_map[0].level1 = 0;
-		bitval_to_rmt_map[0].duration0 = T0H / (RMT_DURATION_NS * DIVIDER);
-		bitval_to_rmt_map[0].duration1 = T0L / (RMT_DURATION_NS * DIVIDER);
-
-		// RMT config for bit val 1
-		bitval_to_rmt_map[1].level0 = 1;
-		bitval_to_rmt_map[1].level1 = 0;
-		bitval_to_rmt_map[1].duration0 = T1H / (RMT_DURATION_NS * DIVIDER);
-		bitval_to_rmt_map[1].duration1 = T1L / (RMT_DURATION_NS * DIVIDER);
+	RMT.int_ena.val |= BIT(24 + mRMT_channel); // set ch*_tx_thr_event
+	RMT.int_ena.val |= BIT(mRMT_channel * 3); // set ch*_tx_end
     }
 
     virtual uint16_t getMaxRefreshRate() const { return 400; }
 
 protected:
 
-	virtual void showPixels(PixelController<RGB_ORDER> & pixels)
-	{
-		esp_intr_alloc(ETS_RMT_INTR_SOURCE, 0, handleInterrupt, this, &rmt_intr_handle);
+    virtual void showPixels(PixelController<RGB_ORDER> & pixels)
+    {
+	esp_intr_alloc(ETS_RMT_INTR_SOURCE, 0, handleInterrupt, this, &mRMT_intr_handle);
 
-		buf_len = (pixels.size() * 3) * sizeof(uint8_t);
-		buf_pos = 0;
-		buf_half = 0;
+	// -- Initialize the local state, save a pointer to the pixel data
+	local_pixels = &pixels;
+	mCurPulse = 0;
+	mRGB_channel = 0;
 
-		local_pixels = &pixels;
+	// -- Fill both halves of the buffer
+	copyToRmtBlock_half();
+	copyToRmtBlock_half();
 
-		copyToRmtBlock_half(*this);
+	mTX_sem = xSemaphoreCreateBinary();
 
-		if (buf_pos < buf_len) {
-			// Fill the other half of the buffer block
-			copyToRmtBlock_half(*this);
-		}
+	// -- Start the RMT TX operationb
+	RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst = 1;
+	RMT.conf_ch[mRMT_channel].conf1.tx_start = 1;
 
-		sem = xSemaphoreCreateBinary();
+	// -- Block until done
+	xSemaphoreTake(mTX_sem, portMAX_DELAY);
 
-		RMT.conf_ch[rmt_channel].conf1.mem_rd_rst = 1;
-		RMT.conf_ch[rmt_channel].conf1.tx_start = 1;
+	// -- When we get here, all of the data has been sent
+	vSemaphoreDelete(mTX_sem);
+	mTX_sem = NULL;
 
-		xSemaphoreTake(sem, portMAX_DELAY);
-		vSemaphoreDelete(sem);
-		sem = NULL;
-
-		esp_intr_free(rmt_intr_handle);
+	esp_intr_free(mRMT_intr_handle);
     }
 
-	void initRMTChannel(int rmtChannel)
-	{
-		RMT.apb_conf.fifo_mask = 1;  //enable memory access, instead of FIFO mode.
-		RMT.apb_conf.mem_tx_wrap_en = 1; //wrap around when hitting end of buffer
-		RMT.conf_ch[rmtChannel].conf0.div_cnt = DIVIDER;
-		RMT.conf_ch[rmtChannel].conf0.mem_size = 1;
-		RMT.conf_ch[rmtChannel].conf0.carrier_en = 0;
-		RMT.conf_ch[rmtChannel].conf0.carrier_out_lv = 1;
-		RMT.conf_ch[rmtChannel].conf0.mem_pd = 0;
+    static void handleInterrupt(void *arg)
+    {
+	ClocklessController* c = static_cast<ClocklessController*>(arg);
+	rmt_channel_t rmt_channel = c->mRMT_channel;
 
-		RMT.conf_ch[rmtChannel].conf1.rx_en = 0;
-		RMT.conf_ch[rmtChannel].conf1.mem_owner = 0;
-		RMT.conf_ch[rmtChannel].conf1.tx_conti_mode = 0;    //loop back mode.
-		RMT.conf_ch[rmtChannel].conf1.ref_always_on = 1;    // use apb clock: 80M
-		RMT.conf_ch[rmtChannel].conf1.idle_out_en = 1;
-		RMT.conf_ch[rmtChannel].conf1.idle_out_lv = 0;
+	portBASE_TYPE xHigherPriorityTaskWoken  = 0;
+
+	if (RMT.int_st.val & BIT(24 + rmt_channel)) { // check if ch*_tx_thr_event is set
+	    // -- Interrupt is telling us the RMT is ready for the next set of pulses
+	    c->copyToRmtBlock_half();
+	    RMT.int_clr.val |= BIT(24 + rmt_channel); // set ch*_tx_thr_event
+	}
+	else if ((RMT.int_st.val & BIT(rmt_channel * 3)) && c->mTX_sem) { // check if ch*_tx_end is set
+	    // -- Interrupt is telling us the RMT is done -- release the semaphore
+	    xSemaphoreGiveFromISR(c->mTX_sem, &xHigherPriorityTaskWoken);
+	    RMT.int_clr.val |= BIT(rmt_channel * 3); // set ch*_tx_end
+
+	    if (xHigherPriorityTaskWoken == pdTRUE) {
+		portYIELD_FROM_ISR();
+	    }
+	}
+    }
+
+    void copyToRmtBlock_half()
+    {
+	// -- Fill half of the RMT pulse buffer
+
+	//    The buffer holds 64 total pulse items, so this loop converts
+	//    as many pixels as can fit in half of the buffer (MAX_PULSES =
+	//    32 items). In our case, each pixel consists of three bytes,
+	//    each bit turns into one pulse item -- 24 items per pixel. So,
+	//    each half of the buffer can hold 1 and 1/3 of a pixel.
+
+	//    The member variable mCurPulse keeps track of which of the 64
+	//    items we are writing. During the first call to this method it
+	//    fills 0-31; in the second call it fills 32-63, and then wraps
+	//    back around to zero.
+
+	//    When we run out of pixel data, just fill the remaining items
+	//    with zero pulses.
+
+	uint16_t pulse_count = 0;
+	uint32_t byteval = 0;
+	while (local_pixels->has(1) && pulse_count < MAX_PULSES) {
+	    // -- Cycle through the R,G, and B values in the right order
+	    switch (mRGB_channel) {
+	    case 0:
+			byteval = local_pixels->loadAndScale0();
+			mRGB_channel = 1;
+			break;
+	    case 1:
+			byteval = local_pixels->loadAndScale1();
+			mRGB_channel = 2;
+			break;
+	    case 2:
+			byteval = local_pixels->loadAndScale2();
+			local_pixels->advanceData();
+			local_pixels->stepDithering();
+			mRGB_channel = 0;
+			break;
+	    default:
+			break;
+	    }
+
+	    byteval <<= 24;
+	    // Shift bits out, MSB first, setting RMTMEM.chan[n].data32[x] to the rmt_item32_t value corresponding to the buffered bit value
+	    for (register uint32_t j = 0; j < 8; j++) {
+		uint32_t val = (byteval & 0x80000000L) ? mOne.val : mZero.val;
+		RMTMEM.chan[mRMT_channel].data32[mCurPulse].val = val;
+		byteval <<= 1;
+		mCurPulse++;
+		pulse_count++;
+	    }
 	}
 
-	static void handleInterrupt(void *arg)
-	{
-		ClocklessController* c = static_cast<ClocklessController*>(arg);
-
-		portBASE_TYPE xHigherPriorityTaskWoken  = 0;
-
-		if (RMT.int_st.val & BIT(24 + c->rmt_channel)) { // check if ch*_tx_thr_event is set
-			copyToRmtBlock_half(*c);
-			RMT.int_clr.val |= BIT(24 + c->rmt_channel); // set ch*_tx_thr_event
-		}
-		else if ((RMT.int_st.val & BIT(c->rmt_channel * 3)) && c->sem) { // check if ch*_tx_end is set
-			xSemaphoreGiveFromISR(c->sem, &xHigherPriorityTaskWoken);
-			RMT.int_clr.val |= BIT(c->rmt_channel * 3); // set ch*_tx_end
-
-			if (xHigherPriorityTaskWoken == pdTRUE) {
-				portYIELD_FROM_ISR();
-			}
-		}
+	// -- Fill the remaining items with zero pulses
+	while (pulse_count < MAX_PULSES) {
+	    RMTMEM.chan[mRMT_channel].data32[mCurPulse].val = 0;
+	    mCurPulse++;
+	    pulse_count++;
 	}
 
-	static void copyToRmtBlock_half(ClocklessController& c)
-	{
-		// This fills half an RMT block
-		// When wraparound is happening, we want to keep the inactive half of the RMT block filled
-		uint16_t i, j, offset, len, byteval;
-		static uint8_t rgb_channel = 0;
-
-		offset = c.buf_half * MAX_PULSES;
-		c.buf_half = !c.buf_half;
-
-		len = c.buf_len - c.buf_pos;
-		if (len > (MAX_PULSES / 8))
-			len = (MAX_PULSES / 8);
-
-		if (!len) {
-			if (!c.buf_isDirty) {
-				return;
-			}
-			// Clear the channel's data block and return
-			for (i = 0; i < MAX_PULSES; i++) {
-				RMTMEM.chan[c.rmt_channel].data32[i + offset].val = 0;
-			}
-
-			c.buf_isDirty = 0;
-			return;
-		}
-		c.buf_isDirty = 1;
-
-		for (i = 0; i < len; i++) {
-			switch (rgb_channel) {
-			case 1:
-				byteval = c.local_pixels->loadAndScale1();
-				rgb_channel = 2;
-				break;
-			case 2:
-				byteval = c.local_pixels->loadAndScale2();
-				c.local_pixels->advanceData();
-				c.local_pixels->stepDithering();
-				rgb_channel = 0;
-				break;
-			default:
-				byteval = c.local_pixels->loadAndScale0();
-				rgb_channel = 1;
-				break;
-			}
-
-			// Shift bits out, MSB first, setting RMTMEM.chan[n].data32[x] to the rmt_item32_t value corresponding to the buffered bit value
-			for (j = 0; j < 8; j++, byteval <<= 1) {
-				int bitval = (byteval >> 7) & 0x01;
-				int data32_idx = i * 8 + offset + j;
-				RMTMEM.chan[c.rmt_channel].data32[data32_idx].val = c.bitval_to_rmt_map[bitval].val;
-			}
-
-			// Handle the reset bit by stretching duration1 for the final bit in the stream
-			if (i + c.buf_pos == c.buf_len - 1) {
-				RMTMEM.chan[c.rmt_channel].data32[i * 8 + offset + 7].duration1 =
-					c.TRS / (RMT_DURATION_NS * DIVIDER);
-			}
-		}
-
-		// Clear the remainder of the channel's data not set above
-		for (i *= 8; i < MAX_PULSES; i++) {
-			RMTMEM.chan[c.rmt_channel].data32[i + offset].val = 0;
-		}
-
-		c.buf_pos += len;
-	}
+	// -- When we have filled the back half the buffer, reset the position to the first half
+	if (mCurPulse >= MAX_PULSES*2)
+	    mCurPulse = 0;
+    }
 };
 
 FASTLED_NAMESPACE_END
